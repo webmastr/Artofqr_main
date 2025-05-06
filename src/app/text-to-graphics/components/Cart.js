89c449b733087f50a312c5b152723be3c8eb721f
Ankit8412226 <125825310+Ankit8412226@users.noreply.github.com>
2025-05-07 01:29:36 +0530

    Api fiexs

--- src/app/text-to-graphics/components/Cart.js
+++ src/app/text-to-graphics/components/Cart.js
@@ -207,7 +207,7 @@ const Cart = ({ cart, setCart, setActiveTab, orderPlaced, setOrderPlaced }) => {
 
       // Call your API to get shipping rates
       const response = await axios.post(
-        "https://artDiamond QR-backend.vercel.app/uploadImage/shipping/rates",
+        "https://artqr-backend.vercel.app/uploadImage/shipping/rates",
         {
           recipient,
           items,
@@ -291,7 +291,7 @@ const Cart = ({ cart, setCart, setActiveTab, orderPlaced, setOrderPlaced }) => {
 
       // Call Stripe checkout endpoint
       const response = await axios.post(
-        "https://artDiamond QR-backend.vercel.app/stripe/create-checkout-session",
+        "https://artqr-backend.vercel.app/stripe/create-checkout-session",
         {
           customer,
           items: lineItems,
